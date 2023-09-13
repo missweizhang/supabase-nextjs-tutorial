cdea55bef995ac3884d2c847c5f65f9c703340c9
Wei Zhang <missweizhang@gmail.com>
2023-09-13 19:32:21 -0400

    fix redirect url

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -2,9 +2,17 @@
 import { Auth } from '@supabase/auth-ui-react';
 import { ThemeSupa } from '@supabase/auth-ui-shared';
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { useEffect, useState } from 'react';
 
 export default function AuthForm() {
   const supabase = createClientComponentClient<Database>();
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  if (!hasMounted) return;
 
   return (
     <Auth
@@ -14,7 +22,7 @@ export default function AuthForm() {
       theme="dark"
       showLinks={false}
       providers={[]}
-      redirectTo="http://localhost:3000/auth/callback"
+      redirectTo={`${location.origin}/auth/callback`}
     />
   );
 }
