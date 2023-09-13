9aea7cd50dc1b0201fee70b6103af4adff3b7d42
Wei Zhang <missweizhang@gmail.com>
2023-09-13 17:36:02 -0400

    tutorial steps

---
+++ src/app/account/page.tsx
@@ -0,0 +1,15 @@
+import AccountForm from '@/components/AccountForm';
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+
+export const dynamic = 'force-dynamic';
+
+export default async function Account() {
+  const supabase = createServerComponentClient<Database>({ cookies });
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  return <AccountForm session={session} />;
+}
